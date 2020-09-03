6540d3c9a109010bb4fbc579e730d66b6f7e0249
lmzyzylkh <lmzyzylkh@gmail.com>
2020-09-03 19:51:05 +0800

    Update Chapter2.java

--- Chapter2.java
+++ Chapter2.java
@@ -1,26 +1,26 @@
 import java.util.*;
 
+class HelloWorld {
+    static void printHelloWorld(){
+        System.out.println("Hello, World");
+    }
+}
+
+/** this is a note */
+class GetParameter {
+    static void getParameter(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input first parameter");
+        String p1 = sc.nextLine();
+        System.out.println("input second parameter");
+        String p2 = sc.nextLine();
+        System.out.println("input third parameter");
+        String p3 = sc.nextLine();
+        System.out.println(p1 + "," + p2 + "," + p3);
+    }
+}
+
 public class Chapter2 {
-    public static class HelloWorld {
-        static void printHelloWorld(){
-            System.out.println("Hello, World");
-        }
-    }
-
-    /** this is a note */
-    public static class GetParameter {
-        static void getParameter(){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("input first parameter");
-            String p1 = sc.nextLine();
-            System.out.println("input second parameter");
-            String p2 = sc.nextLine();
-            System.out.println("input third parameter");
-            String p3 = sc.nextLine();
-            System.out.println(p1 + "," + p2 + "," + p3);
-        }
-    }
-
     public static void main(String[] args) {
         HelloWorld.printHelloWorld();
         GetParameter.getParameter();
